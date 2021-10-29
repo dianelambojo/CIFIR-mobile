dd95702a403794cf58186310089265cb91b75a9c
Diane Isabelle Lambojo <72972840+dianelambojo@users.noreply.github.com>
2021-10-29 16:38:09 +0800

    components folder with log in sample

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>pink para masaya ang buhay</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'purple',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box } from 'native-base';
+
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>pink para masaya</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Login> </Login>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
